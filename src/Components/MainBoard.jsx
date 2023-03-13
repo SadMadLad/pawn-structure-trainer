1849316f569be0f164437895138521ec75af4d58
Saad Azam <zeroinside4u@gmail.com>
2023-03-13 15:55:23 +0500

    Lift the state up for MainBoard in SharedBoard

--- src/Components/MainBoard.jsx
+++ src/Components/MainBoard.jsx
@@ -5,49 +5,7 @@ import { useState } from 'react'
 import Horizontal from './Horizontal'
 import Vertical from './Vertical'
 
-export default function MainBoard({ setPawnFen }) {
-  const [game, setGame] = useState(new Chess())
-
-  const fetchPawnPositions = board => {
-    const pawns = {}
-
-    board.forEach(row => {
-      row.forEach(square => {
-        if (square) {
-          if (square.type === 'p') {
-            pawns[square.square] = `${square.color}${square.type.toUpperCase()}`
-          }
-        }
-      })
-    })
-    return pawns
-  }
-
-  const updateBoard = (move = false, undo = false) => {
-    const gameCopy = new Chess()
-    gameCopy.loadPgn(game.pgn())
-    if (move) { gameCopy.move(move) }
-    if (undo) { gameCopy.undo() }
-
-    const pawnPositions = fetchPawnPositions(gameCopy.board())
-
-    setGame(gameCopy)
-    setPawnFen(pawnPositions)
-  }
-
-  const makeMove = (sourceSquare, targetSquare) => {
-    const move = { from: sourceSquare, to: targetSquare }
-    try {
-      updateBoard(move);
-    } catch (err) {
-      console.log(err)
-      console.log('Invalid Move')
-    }
-  }
-
-  const popHistory = () => {
-    updateBoard(false, true);
-  }
+export default function MainBoard({ makeMove, game }) {
 
   return (
     <div>
@@ -63,9 +21,6 @@ export default function MainBoard({ setPawnFen }) {
         <Vertical />
       </div>
       <Horizontal />
-      <div className='flex justify-end items-center'>
-        <button className="bg-gray-400 hover:bg-gray-700 text-white font-bold px-5 py-3 rounded my-5" onClick={popHistory}>Undo Move</button>
-      </div>
     </div>
   )
 }
--- src/Components/SharedBoard.jsx
+++ src/Components/SharedBoard.jsx
@@ -1,16 +1,65 @@
 import { useState } from "react";
+import { Chess } from "chess.js";
 
 import MainBoard from "./MainBoard";
 import PawnBoard from "./PawnBoard";
 
 export default function SharedBoard() {
   const [pawnFen, setPawnFen] = useState('8/pppppppp/8/8/8/8/PPPPPPPP/8 w - - 1 1');
+  const [game, setGame] = useState(new Chess())
+
+  const fetchPawnPositions = board => {
+    const pawns = {}
+
+    board.forEach(row => {
+      row.forEach(square => {
+        if (square) {
+          if (square.type === 'p') {
+            pawns[square.square] = `${square.color}${square.type.toUpperCase()}`
+          }
+        }
+      })
+    })
+    return pawns
+  }
+
+  const updateBoard = (move = false, undo = false) => {
+    const gameCopy = new Chess()
+    gameCopy.loadPgn(game.pgn())
+    if (move) { gameCopy.move(move) }
+    if (undo) { gameCopy.undo() }
+
+    const pawnPositions = fetchPawnPositions(gameCopy.board())
+
+    setGame(gameCopy)
+    setPawnFen(pawnPositions)
+  }
+
+  const makeMove = (sourceSquare, targetSquare) => {
+    const move = { from: sourceSquare, to: targetSquare }
+    try {
+      updateBoard(move);
+    } catch (err) {
+      console.log(err)
+      console.log('Invalid Move')
+    }
+  }
+
+  const popHistory = () => {
+    updateBoard(false, true);
+  }
 
   return (
+    <>
     <div className="py-5 flex flex-col sm:flex-row justify-around">
-      <MainBoard setPawnFen={setPawnFen} />
+      <MainBoard game={game} makeMove={makeMove} />
       <br />
       <PawnBoard fen={pawnFen} />
+      <br />
     </div>
+    <div className='flex justify-center items-center'>
+        <button className="bg-gray-400 hover:bg-gray-700 text-white font-bold px-5 py-4 rounded" onClick={popHistory}>Undo Move</button>
+    </div>
+    </>
   );
 }
