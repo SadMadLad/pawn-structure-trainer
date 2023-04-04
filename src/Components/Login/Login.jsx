d59ec8a443b5d2b7128b2213ddbafa15042b2aef
Saad Azam <zeroinside4u@gmail.com>
2023-04-04 12:43:18 +0500

    Revert back changes

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,10 +3,15 @@ import Form from '../Shared/Form';
 export default function Login() {
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div>
-        <h1 className="text-3xl text-center font-black text-white">Login</h1>
-        <Form pageType={'login'} />
+    <div className="w-screen h-screen flex">
+      <div className="w-1/2 h-full bg-teal-500">
+        <img src="/pawn.jpg" className="w-full h-full object-cover"/>
+      </div>
+      <div className="w-1/2 h-full flex justify-center items-center">
+        <div>
+          <h1 className="text-3xl text-center font-black text-white">Login</h1>
+          <Form pageType={'login'} />
+        </div>
       </div>
     </div>
   )
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -3,10 +3,15 @@ import Form from '../Shared/Form';
 export default function Register() {
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div>
-        <h1 className="text-4xl font-black text-center text-white mb-5">Register</h1>
-        <Form pageType={'register'} />
+    <div className="w-screen h-screen flex">
+      <div className="w-1/2 h-full bg-teal-500">
+        <img src="/pawn.jpg" className="w-full h-full object-cover"/>
+      </div>
+      <div className="w-1/2 h-full flex justify-center items-center">
+        <div>
+          <h1 className="text-3xl text-center font-black text-white">Register</h1>
+          <Form pageType={'register'} />
+        </div>
       </div>
     </div>
   )
